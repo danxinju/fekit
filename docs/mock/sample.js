34a7a9ab8ec591b8f435e325d35c7b32c414de41
Ice White <yashiro1899@gmail.com>
2014-09-02 10:59:22 +0800

    1409626762

--- docs/mock/sample.js
+++ docs/mock/sample.js
@@ -1,7 +1,7 @@
 module.exports = {
     "/exact/match/1": "exact.json",
     "/exact/match/2": "exact.mockjson",
-    "/exact/match/3": "https://raw.githubusercontent.com/rinh/fekit/penguin/docs/mock/exact.json",
+    "/exact/match/3": "https://raw.githubusercontent.com/rinh/fekit/master/docs/mock/exact.json",
     "/exact/match/4": "exact.js",
     rules: [{
         pattern: "/exact/match/5",
